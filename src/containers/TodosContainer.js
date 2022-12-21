752fcd33a7caa36a73c20b01ba3371d6239dbc81
EmilyJarecki <jareckiemily@yahoo.com>
2022-12-20 19:32:03 -0600

    Sprint2: Containers- completed.

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const TodosContainer = () => {
-  return (
-    <h2>
-        I am the TodosContainer page
-    </h2>
-  )
-}
+    return (
+      <div className='todosContainer'>
+        <h2>This is the todos container</h2>
+      </div>
+    );
+};
 
-export default TodosContainer
+export default TodosContainer;
